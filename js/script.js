abebdba6e942f5e2b5b01417682c2342d6841698
Andrea Bevoni <andreabevoni@gmail.com>
2020-11-20 14:15:24 +0100

    Commit iniziale

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Creiamo BoolzApp:
+// una copia della webApp di whatsApp, o almeno in parte, le funzionalità come visto non saranno complete ma saranno concentrate sulla gestione delle chat e dei messaggi.
+// Ci saranno varie milestones di release del progetto, una per giorno di lavoro.
+// Milestone 1: creare la struttura html e il layout completi.
