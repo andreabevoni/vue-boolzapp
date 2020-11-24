0fb59a2f746919d6141bba78a3409d759f0c40f4
Andrea Bevoni <andreabevoni@gmail.com>
2020-11-24 13:50:13 +0100

    Aggiunta funzione per inviare in messaggio (lato utente)

--- js/script.js
+++ js/script.js
@@ -9,11 +9,25 @@
 // con header con dati relativi a chat attiva;
 // relativa chat in elenco rimane selezionata
 
+// Milestone 2: inserimento msg in chat e relativa risposta:
+// l’utente può scrivere nel campo di input in basso;
+// al click sull’invio succedono 2 cose:
+// 1. il mio msg viene inviato alla chat relativa;
+// 2. ottengo un msg di risposta automatico dopo 1 secondo;
+// tutto ciò viene agganciato/creato solo nella chat attiva;
+// possibili BONUS:
+// l'invio messaggio funziona sia al click sull’elemento in pagina, sia dal tasto ENTER della tastiera;
+// metto data e ora reali;
+// scrollo la chat per far vedere i nuovi msg;
+
+
 // istanzio Vue e definisco i data
 var app = new Vue({
   el: "#app",
   data: {
     selezionato: 0,
+    userText: "",
+
     contatti: [
       {
         nome: "Michele",
@@ -150,6 +164,21 @@ var app = new Vue({
     // funzione per selezionare il profilo amico cliccato
     selectProfile: function(i) {
       this.selezionato = i;
+    },
+    // funzione che aggiunge un nuovo messaggio utente alla lista
+    addChat: function() {
+      // controllo che la casella di input non sia vuota
+      if (this.userText != "") {
+        // creo il nuovo oggetto da pushare
+        let newMessage = {
+          testo: this.userText,
+          data: "12/11/2020 13:11:43",
+          inviato: true
+        }
+        // pusho l'oggetto nella chat selezionata e resetto la casella di input
+        this.contatti[this.selezionato].chat.push(newMessage);
+        this.userText = "";
+      }
     }
   }
 });
